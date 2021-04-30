5b391ca6cfe497a3f216d435da2c85f46621e746
aucoeur <aucoeur.ngo@students.makeschool.com>
2021-04-29 19:06:53 -0700

    Add new channels and subscription

--- server.js
+++ server.js
@@ -8,41 +8,76 @@ const typeDefs = gql`
 		date: String!
 	}
 
+  type Channel {
+    name: String
+    posts: [Post!]!
+  }
+
 	type Query {
-		posts: [Post!]!
+		posts(channel: String!): [Post!]!
+    channels: [Channel]!
 	}
 
 	type Mutation {
-		addPost(message: String!): Post!
+		addPost(name: String!, message: String!) : Post!
+    addChannel(name: String!) : Channel!
 	}
 
 	type Subscription {
 		newPost: Post!
+    newChannel: Channel!
+
 	}
 `
 
-const data = [
-	{ message: 'hello world', date: new Date() }
+const channels = [
+	{
+    name: 'general',
+    posts: [
+      { message: 'hello world',
+        date: new Date() }
+    ]
+  },
+  {
+    name: 'cats',
+    posts: [
+      { message: 'meow world',
+        date: new Date() },
+      { message: 'hiss',
+        date: new Date() },
+    ]
+  },
 ]
 
 const resolvers = {
 	Query: {
-		posts: () => {
-			return data
-		}
+		posts: ({ channel }) => {
+			return channels.filter(c => c.name === channel)
+		},
+    channels: () => {
+      return channels
+    }
 	},
 	Mutation: {
 		addPost: (_, { message }) => {
 			const post = { message, date: new Date() }
-			data.push(post)
-			pubsub.publish('NEW_POST', { newPost: post }) // Publish!
+			channels.push(post)
+			pubsub.publish('NEW_POST', { newPost: post })
 			return post
-		}
+		},
+    addChannel: (_, { name }) => {
+      const channel = { name }
+      pubsub.publish('NEW_CHANNEL', { newChannel: channel })
+      return channel
+    }
 	},
 	Subscription: {
 		newPost: {
 			subscribe: () => pubsub.asyncIterator('NEW_POST')
-		}
+		},
+    newChannel: {
+      subscribe: () => pubsub.asyncIterator('NEW_CHANNEL')
+    }
 	}
 }
 
